870d6123660decc4387873186cd5d43ae20b9da7
vaberry <vincent.berry11@gmail.com>
2023-04-05 00:10:16 -0400

    u

--- templates/app/views.py
+++ templates/app/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.base import View, TemplateView
 
-class Home(TemplateView):
-    template_name='home.html'
+# class Home(TemplateView):
+#     template_name='home.html'
+
+class Home(View):
+    def get(self, request, *args, **kwargs):
+        if request.method == "GET":
+            #delete pass and add code here
+            pass
+        return render(request, 'home.html')
